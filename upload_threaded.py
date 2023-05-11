aacd033cf3334b0b80301e2826687326ae99c890
TeH_Venom <31778205+TehVenomm@users.noreply.github.com>
2023-05-11 11:58:50 -0300

    Update upload_threaded.py

--- upload_threaded.py
+++ upload_threaded.py
@@ -25,7 +25,7 @@ def upload_torchies(x, y, repo_name, is_safetensor):
 
 threads = []
 
-for i = 1 to file_nbr:
+for i in range(1, file_nbr):
     threads.append(threading.Thread(upload_torchies, [i, file_nbr, repo_name]))
   
 if (is_safetensor):
